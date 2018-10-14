a87777ad364bd64f089648d5e20b20948b7f37ae
Stephen Yao <stephenyao95@gmail.com>
2018-10-14 00:26:33 -0600

    Set up title, weather, etc and basic search component

--- App.js
+++ App.js
@@ -1,23 +1,69 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	KeyboardAvoidingView,
+	Platform,
+	TextInput,
+	View
+} from 'react-native';
+
+import SearchInput from './components/SearchInput';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<KeyboardAvoidingView style={styles.container} behavior="padding">
+				<View style={styles.container}>
+					<Text style={[styles.largeText, styles.textStyle]}>
+						San Francisco
+					</Text>
+					<Text style={[styles.smallText, styles.textStyle]}>
+						Light Cloud
+					</Text>
+					<Text style={[styles.largeText, styles.textStyle]}>
+						24°
+					</Text>
+
+					<SearchInput placeholder="Search any city" />
+				</View>
+			</KeyboardAvoidingView>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	textStyle: {
+		textAlign: 'center',
+		...Platform.select({
+			ios: {
+				fontFamily: 'AvenirNext-Regular'
+			},
+			android: {
+				fontFamily: 'Roboto'
+			}
+		})
+	},
+	largeText: {
+		fontSize: 44
+	},
+	smallText: {
+		fontSize: 18
+	},
+	textInput: {
+		backgroundColor: '#666',
+		color: 'white',
+		height: 40,
+		width: 300,
+		marginTop: 20,
+		marginHorizontal: 20,
+		paddingHorizontal: 10,
+		alignSelf: 'center'
+	}
 });
